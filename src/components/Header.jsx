9aad79e50e1c63c328c25210ea24488288c3352c
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-30 22:19:16 +0530

    Add custom CSS with Poppins font and daisyUI theme configuration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,11 @@ const Header = () => {
   return (
     <div>
       <img className="mx-auto py-5" src={logo} alt="" />
-      <p className="text-center">Journalism Without Fear or Favour</p>
-      <p className="text-center">
-        <span className="font-bold">Sunday</span> , November 27, 2025
+      <p className="text-center text-accent">
+        Journalism Without Fear or Favour
+      </p>
+      <p className="text-center text-accent">
+        <span className="font-bold text-black">Sunday</span> , November 27, 2025
       </p>
     </div>
   );
