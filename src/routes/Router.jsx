b4770f879012ad1619ef0df803e3622524eaa15f
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-30 21:14:18 +0530

    Add React router configuration

---
+++ src/routes/Router.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2>Home Layout</h2>,
+  },
+  {
+    path: "/auth",
+    element: <h2>Authentication Layout</h2>,
+  },
+  {
+    path: "/news",
+    element: <h2>News Layout</h2>,
+  },
+  {
+    path: "/*",
+    element: <h2>Error 404 </h2>,
+  },
+]);
+
+export default router;
