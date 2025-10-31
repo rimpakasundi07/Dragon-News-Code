69cd5529eba93018ed73310d9641fb09fb738fc4
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-31 17:54:40 +0530

    feat: add Navbar component with navigation and login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router";
+import user from "../assets/user.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-between items-center py-10">
+      <div className=""></div>
+      <div className="nav flex gap-5 ">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/carrer">Career</NavLink>
+      </div>
+      <div className="login-btn flex justify-center gap-4 items-center">
+        <img src={user} alt="" />
+        <button
+          className="bg-gray-600 btn rounded px-5 py-2 font-bold text-white
+        "
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
