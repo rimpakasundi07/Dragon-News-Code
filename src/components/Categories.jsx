e17c4fb9889d14a9c98ab74a0d4dda17a41a1ddc
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-31 21:57:01 +0530

    Add Categories component with React use hook for data fetching

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,24 @@
 import React, { use } from "react";
+import { NavLink } from "react-router";
 const categoryPromise = fetch("/categories.json").then((res) => res.json());
 
 const Categories = () => {
   const categories = use(categoryPromise);
   return (
     <div>
-      <h2 className="font-bold">All Categories ({categories.length}) </h2>
+      <h2 className="font-bold text-center ">
+        All Categories ({categories.length}){" "}
+      </h2>
+      <div className="grid grid-cols-1 mt-5 gap-4 ">
+        {categories.map((category) => (
+          <NavLink
+            key={category.id}
+            className={"btn bg-base-100 border-0 hover:bg-base-300 "}
+          >
+            {category.name}
+          </NavLink>
+        ))}
+      </div>
     </div>
   );
 };
