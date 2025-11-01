e9023b6d60b57544d6b8012bb3bacbbe6480c0fd
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-01 18:15:30 +0530

    Add CategoryNews component with category filtering

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,12 +1,24 @@
-import React from "react";
-import { useParams } from "react-router";
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
   const { id } = useParams();
-  console.log(id);
+  const data = useLoaderData();
+
+  const [CategoryNews, setCategoryNews] = useState([]);
+
+  //console.log(id, data);
+  useEffect(() => {
+    const filteredNews = data.filter((news) => news.category_id == id);
+    console.log(filteredNews);
+    setCategoryNews(filteredNews);
+  }, [data, id]);
   return (
     <div>
-      <p className="text-center lg:text-4xl">Category News - {id} </p>
+      <p className="text-center lg:text-4xl">
+        {" "}
+        Total {CategoryNews.length} Found{" "}
+      </p>
     </div>
   );
 };
