ea8531495bfbdebcd211804d0cd8b304f74c13e8
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-01 18:56:57 +0530

    Update CategoryNews component with special filtering logic

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -9,15 +9,25 @@ const CategoryNews = () => {
 
   //console.log(id, data);
   useEffect(() => {
-    const filteredNews = data.filter((news) => news.category_id == id);
-    console.log(filteredNews);
-    setCategoryNews(filteredNews);
+    if (id == "0") {
+      setCategoryNews(data);
+      return;
+    } else if (id == "1") {
+      const filteredNews = data.filter(
+        (news) => news.others.is_today_pick == true
+      );
+      setCategoryNews(filteredNews);
+    } else {
+      const filteredNews = data.filter((news) => news.category_id == id);
+      console.log(filteredNews);
+      setCategoryNews(filteredNews);
+    }
   }, [data, id]);
   return (
     <div>
       <p className="text-center lg:text-4xl">
         {" "}
-        Total {CategoryNews.length} Found{" "}
+        Total {CategoryNews.length} News found{" "}
       </p>
     </div>
   );
