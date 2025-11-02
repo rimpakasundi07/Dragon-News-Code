13e6d457de83f4d2e4f00022c8231397a7833bbb
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-02 20:25:51 +0530

    add login page

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import HomeLayout from "../layouts/HomeLayout";
 import Home from "../pages/Home";
 import CategoryNews from "../pages/CategoryNews";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -22,6 +23,12 @@ const router = createBrowserRouter([
   {
     path: "/auth",
     element: <h2>Authentication Layout</h2>,
+    children: [
+      {
+        path: "/auth/login",
+        element: <Login></Login>,
+      },
+    ],
   },
   {
     path: "/news",
