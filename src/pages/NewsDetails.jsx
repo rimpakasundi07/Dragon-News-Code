2757f1c6e17ec364c220d9497852df46333f1ce8
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-03 23:25:35 +0530

    Add NewsDetails page component with header and sidebar layout

--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Header from "../components/Header";
+import RightAside from "../components/homelayout/RightAside";
 
 const NewsDetails = () => {
   return (
     <div>
-      <p>News Details</p>
+      <header className="py-3">
+        <Header></Header>
+      </header>
+      <main className="w-11/12 mx-auto grid grid-cols-12 py-10 gap-5">
+        <section className="col-span-9">
+          <h2 className="font-bold mb-5">News Details </h2>
+        </section>
+        <aside className="col-span-3">
+          <RightAside></RightAside>
+        </aside>
+      </main>
     </div>
   );
 };
